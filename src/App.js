0bcc9054495e654f8d240bac07e43202429c441e
salehowes12 <s12326654@stu.najah.edu>
2024-12-31 20:26:17 +0200

    1

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@
 import ProfilePages from "./components/Pages/ProfilePages";
 
 
-import HomePages from "./components/Pages/HomePages";
+
 import DiabetesDefinition from "./components/Pages/DiabetesDefinition";
 import Type2 from "./components/Pages/Type2";
 import Type1 from "./components/Pages/Type1";
-import Nav from "./components/Nav";
-import Hheadar from "./components/Hheadar";
-import Footer from "./components/Footer";
 import TipsPages from "./components/Pages/TipsPages"
 import TablePage from "./components/Pages/TablePage";
 
 function App() {
   return (
-
-  <HomePages/>
+<div>
+<HomePages/>
    <ProfilePages/>
 
 
@@ -29,6 +26,8 @@ function App() {
  
     
     <TablePage/>
+</div>
+ 
    
   )
 
