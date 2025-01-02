8623a6f231212f3a500a5219fa7c20be3741b23e
ZeinaDawod <s12325937@stu.najah.edu>
2025-01-02 21:29:56 +0300

    zeina

--- src/components/Videocanva.js
+++ src/components/Videocanva.js
@@ -1,7 +1,12 @@
+import "./Videocanva";
 const Videocanva =()=>{
    return(
-<div class="video-container mb-4">
-            <video width="100%" autoplay loop muted></video>
+<div className="video-container mb-4">
+            <video width="100%" autoplay loop muted>
+            <source src="images/Health and Strength.mp4" type="video/mp4">
+                </source>
+
+            </video>
                 
         </div>
 
